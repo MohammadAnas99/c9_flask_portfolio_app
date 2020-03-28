123e59b7e0cbd0a1e0f6cb709959bd07ec73a0ed
MohammadAnas99 <60170848+MohammadAnas99@users.noreply.github.com>
2020-03-28 12:49:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	return render_template('index.html', name=name)
+	new name=name + "likes to eat mangoes"
+	return render_template('index.html', name=new name)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
